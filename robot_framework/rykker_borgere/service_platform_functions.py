17411ed36c1ebc2444f7a14b7a9b9a73d934c7da
kri-bak <kriba@aarhus.dk>
2026-06-29 14:46:36 +0200

    fixed erroneous reference in production

--- robot_framework/rykker_borgere/service_platform_functions.py
+++ robot_framework/rykker_borgere/service_platform_functions.py
@@ -11,6 +11,7 @@ from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConn
 from requests.exceptions import HTTPError, ConnectionError as RequestsConnectionError, Timeout
 
 from robot_framework import config
+from robot_framework.rykker_borgere import util
 
 
 def get_kombit_access(orchestrator_connection: OrchestratorConnection):
@@ -76,7 +77,7 @@ def send_sms(kombit_access: KombitAccess, recipient_cpr: str, language: str = "d
     )
 
     # Load appropriate SMS text based on language
-    sms_file = "rykker_borgere/templates/sms_text_da.txt" if language == "da" else "rykker_borgere/templates/sms_text_en.txt"
+    sms_file = util.TEMPLATES_DIR / ("sms_text_da.txt" if language == "da" else "sms_text_en.txt")
     with open(sms_file, "r", encoding="utf-8") as file:
         sms_text = file.read()
 
